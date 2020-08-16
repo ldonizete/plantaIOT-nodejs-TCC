deb0dde85c5da7a14139e17f3577dfaa47a76dee
Lucas Donizete <lcs.dgl@outlook.com>
2020-08-16 06:53:56 -0300

    customer

--- src/models/customer.js
+++ src/models/customer.js
@@ -4,20 +4,31 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    minlength: 3,
+    maxlength: 100
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    minlength: 3,
+    maxlength: 100
   },
   password: {
     type: String,
-    required: true
+    required: true,
+    minlength: 3,
+    maxlength: 100
   },
   active: {
     type: Boolean,
     required: true,
     default: true
+  }, 
+  administrator: {
+    type: Boolean,
+    required: true,
+    default: false
   }, 
   date: {
     type: Date,
